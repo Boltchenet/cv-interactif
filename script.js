a88136d939dd1b1e14011d9d6ae76b7f8a075b9c
Boltchenet <hugotianyi.ma@gmail.com>
2025-03-23 18:57:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Exemple pour changer la couleur de fond
-    const button = document.createElement("button");
-    button.textContent = "Changer la couleur de fond";
-    document.body.appendChild(button);
-
-    button.addEventListener("click", function() {
-        document.body.style.backgroundColor = "#E0E0E0";
-    });
-
     // Fonction pour ouvrir/fermer la sidebar
     function toggleSidebar() {
         const sidebar = document.getElementById('sidebar');
         const currentPosition = sidebar.style.left;
+        
+        // Vérifie si la sidebar est ouverte ou fermée
         if (currentPosition === '0px') {
             sidebar.style.left = '-250px';  // Ferme la sidebar
         } else {
