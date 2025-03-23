328d04da622a1d96ebb775dece8634fb046bdfa1
Boltchenet <hugotianyi.ma@gmail.com>
2025-03-23 20:44:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Fonction pour ouvrir/fermer la sidebar
     function toggleSidebar() {
         const sidebar = document.getElementById('sidebar');
-        const currentPosition = sidebar.style.left;
         
-        // Vérifie si la sidebar est ouverte ou fermée
-        if (currentPosition === '0px') {
-            sidebar.style.left = '-250px';  // Ferme la sidebar
-        } else {
-            sidebar.style.left = '0px';    // Ouvre la sidebar
-        }
+        // Ajoute ou enlève la classe "open" pour faire apparaître ou disparaître la sidebar
+        sidebar.classList.toggle('open');
     }
 
     // Associe la fonction au clic sur l'icône hamburger
